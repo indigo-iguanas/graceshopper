df1511ccd57851a4b3c0a31b34b1826d6c1229b2
zacharyAD <zacharyadcoding@gmail.com>
2019-02-27 12:08:30 -0500

    added get and post route into the cart model, have not tested at this point.

---
+++ server/api/cart.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const {Order} = require('../db/models/index.js')
+module.exports = router
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const cartItems = await Order.findAll({
+      where: {
+        userId,
+        status: 'inCart'
+      }
+    })
+    res.json(cartItems)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/:id', async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const order = await Order.create({...req.body, userId})
+    res.json(order)
+  } catch (err) {
+    next(err)
+  }
+})
