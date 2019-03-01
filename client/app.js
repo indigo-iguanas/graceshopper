d3d0c712c1ef2b21f4b95c1d9e917517608f0141
Erina Kii <erina.kii92@gmail.com>
2019-02-28 20:21:53 -0500

    fixed issue for fuzzy component rendering

--- client/app.js
+++ client/app.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
-import Emotions from './components/emotions'
+// import Emotions from './components/emotions'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
-      <Emotions />
+      {/* <Emotions /> */}
     </div>
   )
 }
--- client/routes.js
+++ client/routes.js
@@ -24,8 +24,8 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route exact path="/home" component={UserHome} />
-            <Route exact path="/emotions" component={Emotions} />
+            <Route path="/home" component={UserHome} />
+            <Route path="/emotions" component={Emotions} />
             <Route path="/cart" component={Cart} />
           </Switch>
         )}
