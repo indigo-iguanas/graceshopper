12df881d79abcaa0a9d9293c98f98153293b6e66
David H. Friedman <dhf@panix.com>
2019-02-28 19:27:19 -0500

    route to purchase items in cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -27,3 +27,35 @@ router.post('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.put('/', async (req, res, next) => {
+  try {
+    if (
+      !req.session.passport ||
+      !req.session.passport.hasOwnProperty('user') ||
+      req.body.userId.id !== req.session.passport.user
+    ) {
+      res.status(401).end()
+    } else {
+      const [count, _rows] = await Order.update(
+        {
+          date: new Date(),
+          status: 'purchased'
+        },
+        {
+          where: {
+            userId: req.body.userId.id,
+            status: 'inCart'
+          }
+        }
+      )
+      if (count === 0) {
+        res.status(412).json('No items in cart')
+      } else {
+        res.status(204).end()
+      }
+    }
+  } catch (err) {
+    next(err)
+  }
+})
