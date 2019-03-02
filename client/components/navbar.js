a48d2fa92d23c17ac88b157a4ca74aeb1c6e2a2f
David H. Friedman <dhf@panix.com>
2019-03-02 08:39:20 -0500

    change title to "EMOTIONS 'Я US"

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>EMOTIONS 'Я US</h1>
     <nav>
       {isLoggedIn ? (
         <div>
