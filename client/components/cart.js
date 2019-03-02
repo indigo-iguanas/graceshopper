21c34cc9a8e01b30c1b73ca26df5ddec81559453
Erina Kii <erina.kii92@gmail.com>
2019-03-02 15:32:57 -0500

    included master changes into cart.js

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {getCartFromServer, me, deleteLineItemFromServer} from '../store'
+import {
+  getCartFromServer,
+  makePurchase,
+  me,
+  deleteLineItemFromServer
+} from '../store'
 import {withRouter} from 'react-router-dom'
 
 class Cart extends Component {
   constructor(props) {
     super(props)
+    this.purchaseCart = this.purchaseCart.bind(this)
     this.clickHandler = this.clickHandler.bind(this)
   }
 
@@ -13,6 +19,16 @@ class Cart extends Component {
     this.props.fetchUserFromStore()
     const id = this.props.user.id
     this.props.fetchCartFromStore(id)
+  }
+  purchaseCart() {
+    try {
+      this.props.makePurchase(this.props.user)
+      // TODO - how to show this to customer?
+      console.log('CART: purchase succeeded, order id ?')
+    } catch (err) {
+      // TODO - how to show this to customer?
+      console.log('CART: purchase failed')
+    }
   }
 
   clickHandler(id, lineItemId) {
@@ -23,27 +39,32 @@ class Cart extends Component {
     const cart = this.props.cart
     return (
       <div>
+        <button type="button" onClick={this.purchaseCart}>
+          Purchase
+        </button>
         <h1>Your cart</h1>
-        {cart.length > 0 ? (
-          cart.filter(el => el.status === 'inCart').map((el, idx) => {
-            return (
-              <div key={idx}>
-                <h2>{el.emotion.name}</h2>
-                <img src={el.emotion.imageUrl} />
-                <button
-                  type="button"
-                  onClick={() => {
-                    this.clickHandler(this.props.user.id, el.id)
-                  }}
-                >
-                  I DON'T WANT IT!
-                </button>
-              </div>
-            )
-          })
-        ) : (
-          <h1>No Orders In Your Cart Currently!</h1>
-        )}
+        <div className="catalog">
+          {cart.length > 0 ? (
+            cart.filter(el => el.status === 'inCart').map((el, idx) => {
+              return (
+                <div key={idx}>
+                  <h2>{el.emotion.name}</h2>
+                  <img src={el.emotion.imageUrl} />
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.clickHandler(this.props.user.id, el.id)
+                    }}
+                  >
+                    I DON'T WANT IT!
+                  </button>
+                </div>
+              )
+            })
+          ) : (
+            <h1>No Orders In Your Cart Currently!</h1>
+          )}
+        </div>
       </div>
     )
   }
@@ -57,6 +78,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   fetchCartFromStore: id => dispatch(getCartFromServer(id)),
   fetchUserFromStore: () => dispatch(me()),
+  makePurchase: id => dispatch(makePurchase(id)),
   deleteLineItemFromStore: (id, lineItemId) =>
     dispatch(deleteLineItemFromServer(id, lineItemId))
 })
