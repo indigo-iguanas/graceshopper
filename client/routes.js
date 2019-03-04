4aaba07e26d85f4d98c801a1829e0b3635934a30
Erina Kii <erina.kii92@gmail.com>
2019-03-04 10:09:28 -0500

    fixed routing

--- client/routes.js
+++ client/routes.js
@@ -21,9 +21,11 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/" component={Emotions} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
+            <Route path="/" component={Emotions} />
             <Route path="/home" component={UserHome} />
             <Route path="/emotions" component={Emotions} />
             <Route path="/cart" component={Cart} />
