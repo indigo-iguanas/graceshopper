6ded78fe6361cffbee0c3165e9fe81c9c79ad0d5
zacharyAD <zacharyadcoding@gmail.com>
2019-03-05 16:40:34 -0500

    took out unneeded console.logs

--- server/auth/google.js
+++ server/auth/google.js
@@ -21,7 +21,6 @@ module.exports = router
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 } else {
-  console.log('made it to else in google')
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -34,8 +33,6 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
       const googleId = profile.id
       const name = profile.displayName
       const email = profile.emails[0].value
-
-      console.log('C')
 
       User.findOrCreate({
         where: {googleId},
